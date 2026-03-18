6581571b40dbb1c2d770a64e17adbe000291009a
Alexander Ramsey <alexramsey92@gmail.com>
2026-03-18 11:25:26 -0400

    feat: update L7 partner page brand color to green

--- resources/views/frontend/partials/footer.blade.php
+++ resources/views/frontend/partials/footer.blade.php
@@ -73,7 +73,7 @@
                 <div>
                     <h4 class="text-white font-bold mb-4 text-xs uppercase tracking-widest">Partners</h4>
                     <div class="flex flex-col gap-2.5 text-slate-500">
-                        <a href="{{ route('partners.l7') }}" class="hover:text-white transition-colors">L7 Media Labs</a>
+                        <a href="{{ route('partners.l7') }}" class="hover:text-white transition-colors">L7 Labs</a>
                     </div>
                 </div>
 
